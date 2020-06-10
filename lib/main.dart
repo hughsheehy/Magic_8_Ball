1f87f16cfc58d2ff10efd86f20dd7087777ec4f1
hugh sheehy <hughsheehy@users.noreply.github.com>
2020-06-10 12:58:22 +0100

    First commit
    Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Text('Ask Hugh a question.'),
+        backgroundColor: Colors.green.shade800,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballnumber = 1 + Random().nextInt(5);
+          });
+        },
+        child: Image.asset('images/ball$ballnumber.png'),
+      ),
+    );
+  }
+}
